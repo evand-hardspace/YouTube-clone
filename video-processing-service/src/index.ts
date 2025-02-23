369b258150481460ea0ca850e826548d0fc9ecfb
Ivan Kravchenko <ivan.kravchenko217@gmail.com>
2025-02-23 21:40:57 +0100

    Implement video-processing-service

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -1,12 +1,35 @@
 import express from "express";
+import ffmpeg from "fluent-ffmpeg";
 
 const app = express();
-const port = 3000;
+app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.send("Hello world!");
+app.post("/process-video", (req, res) => {
+    // Get path of the input video file from the request body
+
+    const inputFilePath = req.body.inputFilePath;
+    const outputFilePath = req.body.outputFilePath;
+
+    if (!inputFilePath || !outputFilePath) {
+        res.status(400).send("Bad Request: Missing file path.");
+        return;
+    }
+
+    ffmpeg(inputFilePath)
+        .outputOptions("-vf", "scale=-1:360") // 360p
+        .on("end", () => {
+            console.log('Processing finished successfully');
+            res.status(200).send("Video processing finished successfully.");
+        })
+        .on("error", (err) => {
+            console.log(`An error occurred: ${err.message}`);
+            res.status(500).send(`Internal server error: ${err.message}`);
+        })
+        .save(outputFilePath);
+
 });
 
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(
         `Video processing service listening at http://localhost:${port}`
